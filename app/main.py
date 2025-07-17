1c53b9fc3bf5dc477735691508d570e8f9a8680a
joolienhoolien <julienpecquet@gmail.com>
2025-07-17 12:08:49 -0600

    added some handling for different requests.

--- app/main.py
+++ app/main.py
@@ -1,14 +1,27 @@
 import socket  # noqa: F401
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+def build_http_response():
+    pass
 
-    # Uncomment this to pass the first stage
+
+def main():
+    #Create the server on machine. This can open up to allow requests.
     server_socket = socket.create_server(("localhost", 4221))
-    server_socket.accept()[0].send("HTTP/1.1 200 OK\r\n\r\n".encode())
-    server_socket.accept() # wait for client
+
+    #Accept a call from outside the server. Save the other machine so we can reply with an HTTP code
+    client_socket, addr = server_socket.accept()
+    with client_socket:
+        #if the request has no info past the first /, its 200, otherwise its 404
+        data = client_socket.recv(1024)
+        request = data.decode()
+
+        #Parse
+        if request.startswith("GET / HTTP/1.1"):
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+        else:
+            response = "HTTP/1.1 404 OK\r\n\r\n"
+        client_socket.sendall(response.encode())
 
 
 if __name__ == "__main__":
